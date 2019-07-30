f043bc73c3ce1f4411ca87876bf912eee8d7fc1e
WilliansAlb <williansalbertolpez@hotmail.com>
2019-07-30 12:37:49 -0600

    Agregando estilos

--- script.js
+++ script.js
@@ -2,24 +2,37 @@ var palabra = document.getElementById("No1");
 var historial = document.getElementById("historial");
 var tipo = document.getElementById("tipo1");
 
+var error1 = "ERROR(SIMBOLO)";
+var error2 = "ERROR(INICIO NUMERO)";
+var numero1 = "ERROR(DECIMAL)";
+var numero2 = "NUMERO";
+var identificador1 = "IDENTIFICADOR";
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function funcion() {
+    colocarImagen();
     console.log('Taking a break...');
     await sleep(1000);
     reconocedor();
-    console.log('Two seconds later, showing sleep in a loop...');
+    quitarImagen();
 } 
+function colocarImagen(){
+    document.getElementById("icono").src ="cargando1.gif";
+}
+function quitarImagen(){
+    document.getElementById("icono").src ="";
+}
 function reconocedor(){
     var temporal =  false;
     if(palabra.value>0 || palabra.value<0 || palabra.value==0){
         if (palabra.value % 1 == 0){
-            historial.innerHTML = historial.innerHTML + palabra.value + "<br>";
-            tipo.innerHTML = tipo.innerHTML + "NUMERO" + "<br>";
+            historial.innerHTML = historial.innerHTML + palabra.value.fontcolor("blue")  + "<br>";
+            tipo.innerHTML = tipo.innerHTML + numero2.fontcolor("blue") + "<br>";
         } else {
-            historial.innerHTML = historial.innerHTML + palabra.value + "<br>";
-            tipo.innerHTML = tipo.innerHTML + "ERROR" + "<br>";
+            historial.innerHTML = historial.innerHTML + palabra.value.fontcolor("red")  + "<br>";
+            tipo.innerHTML = tipo.innerHTML + numero1.fontcolor("red")+ "<br>";
         }
     } else {
         var abc = "abcdefghijklmnopqrstuvwxyz1234567890";
@@ -38,17 +51,17 @@ function reconocedor(){
         if (temporal==false){
             if (pb.charAt(0)>=0){
                 alert("es un error");
-                historial.innerHTML = historial.innerHTML + palabra.value + "<br>";
-                tipo.innerHTML = tipo.innerHTML + "ERROR" + "<br>";
+                historial.innerHTML = historial.innerHTML + palabra.value.fontcolor("red") + "<br>";
+                tipo.innerHTML = tipo.innerHTML + error2.fontcolor("red") + "<br>";
             }else{
                 alert("es un identificador");
-                historial.innerHTML = historial.innerHTML + palabra.value + "<br>";
-                tipo.innerHTML = tipo.innerHTML + "IDENTIFICADOR" + "<br>";
+                historial.innerHTML = historial.innerHTML + palabra.value.fontcolor("green") + "<br>";
+                tipo.innerHTML = tipo.innerHTML + identificador1.fontcolor("green") + "<br>";
             }
         } else {
             alert("es un error");
-            historial.innerHTML = historial.innerHTML + palabra.value + "<br>";
-            tipo.innerHTML = tipo.innerHTML + "ERROR (SIMBOLO)" + "<br>";
+            historial.innerHTML = historial.innerHTML + palabra.value.fontcolor("red") + "<br>";
+            tipo.innerHTML = tipo.innerHTML + error1.fontcolor("red") + "<br>";
         }
     }
 }
